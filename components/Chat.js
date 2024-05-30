846c5342965e35c596a04a36af1e034abcd8e08d
kurenkov004 <mary.kurenkov@gmail.com>
2024-05-30 14:09:19 -0400

    added message db logic

--- components/Chat.js
+++ components/Chat.js
@@ -1,39 +1,38 @@
+import { orderBy } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { collection, addDoc, onSnapshot, query } from 'firebase/firestore';
 
-const Chat = ({ route, navigation }) => {
-  const { name, background } = route.params; //extract props from navigation
+const Chat = ({ route, navigation, db }) => {
+  const { name, background, _id } = route.params; //extract props from navigation
   const [messages, setMessages] = useState([]); //state to handle new messages being set/ rendered
 
 
   //this is triggered right after the Chat component mounts
   useEffect(() => {
     navigation.setOptions({ title: name }) // changes the title of the navbar to the name the user entered on Start screen
-    setMessages([ //this will make the page load two static messages
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-      {
-        _id: 2,
-        text: 'This is a system message',
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
+    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+    const unsubMessages = onSnapshot(q, (documentSnapshot) => {
+      let newMessages = [];
+      documentSnapshot.forEach(doc => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis())
+        })
+      })
+      setMessages(newMessages);
+    })
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
   }, []);
 
-  //function to handle new messages 
+  //function to handle new messages, saves them to Firestore db by using addDoc()
   const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-  } 
+    addDoc(collection(db, "messages"), newMessages[0])
+  }
 
   //function to customize chat bubble colours
   const renderBubble = (props) => {
@@ -57,8 +56,8 @@ const Chat = ({ route, navigation }) => {
       renderBubble={renderBubble}
       onSend={messages => onSend(messages)}
       user={{
-        _id: 1,
-        name
+        _id: _id,
+        name: name
       }}
      />
      {Platform.OS ==='android' ? <KeyboardAvoidingView behavior='height' /> : null }
